35acb6633ec823ae62bafe9e05e33fbcc794f4bf
lvzongting <lvzongting@gmail.com>
2016-12-21 10:55:58 +1100

    Create read_test_tfrecord.py

---
+++ utils/read_test_tfrecord.py
@@ -0,0 +1,11 @@
+import tensorflow as tf
+
+
+for serialized_example in tf.python_io.tf_record_iterator("train.tfrecord"):
+    example = tf.train.Example()
+    example.ParseFromString(serialized_example)
+
+    image = example.features.feature['img'  ].bytes_list.value
+    label = example.features.feature['label'].int64_list.value
+
+    print label, len(image)
