3d59401c6af9b67e50fb8e970a9c206a361eeeb5
lvzongting <lvzongting@gmail.com>
2016-12-21 11:35:06 +1100

    Create load_tfrecord.py

---
+++ utils/load_tfrecord.py
@@ -0,0 +1,17 @@
+import tensorflow as tf 
+
+def load_tfrecord(filename):                                                                                                                      
+    #gen queue from filename list
+    filename_queue = tf.train.string_input_producer([filename])
+    reader = tf.TFRecordReader()
+    _, serialized_example = reader.read(filename_queue)   #return the name of file
+    features = tf.parse_single_example(serialized_example,
+        features={
+           'label'   : tf.FixedLenFeature([], tf.int64),
+           'img_raw' : tf.FixedLenFeature([], tf.string),
+        })
+    img = tf.decode_raw(features['img_raw'], tf.uint8)
+    img = tf.reshape(img, [227, 227, 3])
+    img = tf.cast(img, tf.float32) * (1. / 255) - 0.5
+    label = tf.cast(features['label'], tf.int32)
+    return img, label
