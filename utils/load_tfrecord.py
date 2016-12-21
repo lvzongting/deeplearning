4c91a3c579eaf9a3d83bdef2a6d2bf7731d9dbff
lvzongting <lvzongting@gmail.com>
2016-12-21 12:05:36 +1100

    Update load_tfrecord.py

--- utils/load_tfrecord.py
+++ utils/load_tfrecord.py
@@ -19,10 +19,10 @@ def load_tfrecord(filename):
     _, serialized_example = reader.read(filename_queue)   #return the name of file
     features = tf.parse_single_example(serialized_example,
         features={
-           'label'   : tf.FixedLenFeature([], tf.int64),
-           'img_raw' : tf.FixedLenFeature([], tf.string),
+           'label' : tf.FixedLenFeature([], tf.int64),
+           'img'   : tf.FixedLenFeature([], tf.string),
         })
-    img = tf.decode_raw(features['img_raw'], tf.uint8)
+    img = tf.decode_raw(features['img'], tf.uint8)
     img = tf.reshape(img, [227, 227, 3])
     img = tf.cast(img, tf.float32) * (1. / 255) - 0.5
     label = tf.cast(features['label'], tf.int32)
