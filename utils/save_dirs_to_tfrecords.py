492d1207b3a6b05972b1a17f26605a040fcd822d
lvzongting <lvzongting@gmail.com>
2016-12-22 12:01:08 +1100

    Update save_dirs_to_tfrecords.py

--- utils/save_dirs_to_tfrecords.py
+++ utils/save_dirs_to_tfrecords.py
@@ -15,21 +15,30 @@ tree of dataset dirs
 ...
 '''
 
-record = tf.python_io.TFRecordWriter('train.tfrecord')
+rate = 1.0/2
+train = True ; test = False
+#train = False; test = True
+
+if train: record = tf.python_io.TFRecordWriter('train.tfrecord')
+if test : record = tf.python_io.TFRecordWriter('test.tfrecord')
 for index,[root, dirs, files] in enumerate(os.walk('.')):
     if index == 0:
         classes = dirs
         continue
-    for sample in files:
+    flag_files = int(len(files)*rate)
+    for num,sample in enumerate(files):
         #227,227 is the size of Alexnet
-        print root + '/' +sample
+        if train & (num > flag_files): continue
+        if test  & (num <=flag_files): continue
+        if train: print 'train: '+ str(index) + ':'+ str(num+1) + '/' + str(len(files))+ ':' + root + '/' +sample
+        if test:  print 'test:  '+ str(index) + ':'+ str(num+1) + '/' + str(len(files))+ ':' + root + '/' +sample
         img = Image.open(root + '/' +sample)
         if img.mode != 'RGB':
             img = img.convert('RGB')
         img = img.resize((227,227)).tobytes()
         example = tf.train.Example(features=tf.train.Features(feature={
-            "label": tf.train.Feature(int64_list=tf.train.Int64List(value=[index])),
-            "img"  : tf.train.Feature(bytes_list=tf.train.BytesList(value=[  img]))
+            "label": tf.train.Feature(int64_list=tf.train.Int64List(value=[index - 1])),
+            "img"  : tf.train.Feature(bytes_list=tf.train.BytesList(value=[  img    ]))
         }))
         record.write(example.SerializeToString())
     #if index == 2:
