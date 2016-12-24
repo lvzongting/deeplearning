011ccb7cbf02383169e51fb6f83ae7956edb4555
lvzongting <lvzongting@gmail.com>
2016-12-24 11:05:14 +1100

    Update Lenet_caltech101_28x28x10.py

--- Lenet_caltech101_28x28x10.py
+++ Lenet_caltech101_28x28x10.py
@@ -1,5 +1,5 @@
 import tensorflow as tf
-from utils.load_tfrecord2 import *
+from utils.load_tfrecords2 import *
 
 x = tf.placeholder(tf.float32, shape=[None, 28, 28, 1])
 y0= tf.placeholder(tf.int32, shape=[None])
@@ -39,12 +39,12 @@ train_step = tf.train.AdamOptimizer(1e-4).minimize(cross_entropy)
 correct_prediction = tf.equal(tf.argmax(y,1), tf.argmax(y_,1))
 accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
 
-#for tr_num,_ in  enumerate(tf.python_io.tf_record_iterator("caltech101/train28x28.tfrecord")):pass
-#for ts_num,_ in  enumerate(tf.python_io.tf_record_iterator("caltech101/test28x28.tfrecord" )):pass
+#for tr_num,_ in  enumerate(tf.python_io.tf_record_iterator("caltech101/train28x28.tfrecords")):pass
+#for ts_num,_ in  enumerate(tf.python_io.tf_record_iterator("caltech101/test28x28.tfrecords" )):pass
 tr_num = 1461; ts_num = 1445
 
-tr_img, tr_label = load_tfrecord("caltech101/train28x28.tfrecord")
-ts_img, ts_label = load_tfrecord("caltech101/test28x28.tfrecord" )
+tr_img, tr_label = load_tfrecords("caltech101/train28x28.tfrecords")
+ts_img, ts_label = load_tfrecords("caltech101/test28x28.tfrecords" )
 tr_img_batch, tr_label_batch = tf.train.shuffle_batch([tr_img, ts_label],batch_size=500,   capacity=2000,min_after_dequeue=1000)
 ts_img_batch, ts_label_batch = tf.train.shuffle_batch([ts_img, ts_label],batch_size=ts_num,capacity=2000,min_after_dequeue=1000)
 
