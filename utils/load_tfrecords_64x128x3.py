104afcc731a7cf921ee0924c60e2a41915b35023
lvzongting <lvzongting@gmail.com>
2016-12-28 17:48:07 +1100

    Add files via upload

---
+++ utils/load_tfrecords_64x128x3.py
@@ -0,0 +1,20 @@
+import tensorflow as tf
+
+def load_tfrecords(filename):
+    #gen queue from filename list
+    filename_queue = tf.train.string_input_producer([filename])
+    reader = tf.TFRecordReader()
+    _, serialized_example = reader.read(filename_queue)   #return the name of file
+    features = tf.parse_single_example(serialized_example,
+        features={
+           'label' : tf.FixedLenFeature([], tf.int64),
+           'img'   : tf.FixedLenFeature([], tf.string),
+        })
+    img = tf.decode_raw(features['img'], tf.uint8)
+    img = tf.reshape(img, [64, 128, 3])
+    img = tf.cast(img, tf.float32) * (1. / 255) - 0.5
+    label = tf.cast(features['label'], tf.int32)
+    return img, label
+
+#if __name__ == '__name__':
+    
